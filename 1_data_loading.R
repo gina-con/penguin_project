834fd27dac502bf21fbf9efe6daa3343457b0d82
eddimd <s1933945@ed.ac.uk>
2025-09-24 15:41:59 +0100

    added code

--- 1_data_loading.R
+++ 1_data_loading.R
@@ -5,6 +5,7 @@ penguin_data <- read.table("data/penguin_data.txt", header = TRUE)
 dim(penguin_data)
 head(penguin_data)
 
+penguin_data
 #i am corruting your code gina
 
 #you are welcome for these useless comments 
