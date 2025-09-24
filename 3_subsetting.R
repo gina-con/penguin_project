b6dbe880d4a1ce1b387f5a06a22a6b55368ad852
gina-con <gcon00@proton.me>
2025-09-24 15:05:41 +0100

    subsetting male data

--- 3_subsetting.R
+++ 3_subsetting.R
@@ -1,3 +1,7 @@
 #subsetting female data
 penguins_female <- subset(penguin_data, sex == "female")
 write_tsv(penguins_female, "results/1_penguin_female_only.txt")
+
+#subsetting male data
+penguins_male <- subset(penguin_data, sex == "male")
+write_tsv(penguins_male, "results/1_penguin_male_only.txt")
